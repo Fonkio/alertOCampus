05917e8a5172341e83471ab36e158522e85d29ea
Azouu <pika.azou@gmail.com>
2020-01-05 00:21:04 +0100

    Ajout classe Utilisateur, Groupe, Serveur, ApplicationServeur et requêtes SQL générales

---
+++ WorkSpace Eclipse/Serveur/src/Utilisateur.java
@@ -0,0 +1,62 @@
+
+public class Utilisateur implements Comparable<Utilisateur> {
+	private int id;
+	private String prenom;
+	private String nom;
+	
+	public Utilisateur(int id, String prenom, String nom) {
+		this.id = id;
+		this.prenom = prenom;
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof Utilisateur) {
+			Utilisateur userToCompare = (Utilisateur) o;
+			return userToCompare.id == id;
+		}
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return 31 * id;
+	}
+
+	@Override
+	public String toString() {
+		return "Utilisateur [id=" + id + ", prenom=" + prenom + ", nom=" + nom + "]";
+	}
+
+	@Override
+	public int compareTo(Utilisateur userToCompare) {
+		int comparaisonNom = nom.compareTo(userToCompare.nom);
+		if (comparaisonNom == 0) {
+			return prenom.compareTo(userToCompare.prenom);
+		} 
+		return comparaisonNom;
+	}
+	
+	
+}
